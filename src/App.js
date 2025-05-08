51d1037d840dfca531976102d1fd899aead0c07e
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-05-08 23:42:59 +0300

    finalupdated

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ const App = () => {
         </ul>
       </nav>
       <main className="h-7/8 w-full flex justify-center items-center relative">
-        <div className="polygon-clip w-full h-6/8 bg-[#095F59] absolute bottom-0 z-10"></div>
-        <div className="circle-clip w-[162px] h-[162px] absolute top-[300px] left-[100px] bg-[#095F59D6] border-[23px] border-[#02B94B1A] z-11 rounded-full"></div>
-        <div className="circle-clip w-[230px] h-[230px] absolute top-[530px] left-[1300px] bg-[#095F59D6] border-[48px] border-[#02B94B1A] z-11 rounded-full"></div>
+        <div
+          className="polygon-clip w-full h-6/8 bg-[#095F59] absolute bottom-0 z-10 
+        "
+        ></div>
+        <div className="circle-clip w-[162px] h-[162px] absolute top-[300px] left-[100px] bg-[#095F59D6] border-[23px] border-[#02B94B1A] z-11 rounded-full hidden md:block"></div>
+        <div className="circle-clip w-[230px] h-[230px] absolute top-[530px] left-[1300px] bg-[#095F59D6] border-[48px] border-[#02B94B1A] z-11 rounded-full hidden md:block"></div>
 
-        <div className="h-4/5 w-1/3  bg-[#FFFFFFE5] z-10 items-start p-10 shadow-lg">
+        <div className="h-4/5 md:w-1/3 w-2/3 bg-[#FFFFFFE5] z-10 items-start p-10 shadow-lg">
           <form className="flex flex-col space-y-5">
             <h3 className="font-bold text-[50px] text-[#095F59D6] w-1/2">
               LOGIN
@@ -29,7 +32,7 @@ const App = () => {
               placeholder="Password"
               className="p-2 border-1 rounded-md border-[#0000004F]"
             />
-            <button className="p-2 bg-[#095F59D6] w-1/2 text-white rounded-md ">
+            <button className="p-2 bg-[#095F59D6] md:w-1/2 text-white rounded-md w-full">
               LOGIN
             </button>
             <div className="flex items-center w-full ">
